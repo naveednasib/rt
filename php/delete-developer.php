501c7cc77271ef46ae5833be09e2b6abed396e98
Nasib <Nasib>
2017-07-03 02:29:25 +0500

    delete php file

---
+++ php/delete-developer.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+  			     include_once('../connection.php');
+
+
+  			 $getId = $_POST['del_id'];
+
+
+  			 // DELETE FROM music WHERE msuic_id ='$music_number
+			$deletequery = "Delete FROM tbl_developer where id = '$getId' ";
+		       
+			$result = mysqli_query($conn, $deletequery );
+			//$result=mysqli_query($deletequery);
+
+	
+			if($result == true) {
+   			echo "success";
+			} else {
+		    echo "NO";
+			}
+									
+
+                ?>
